bc73cf9955f9df7a8c18f678170dd0b6f00003b4
Myriam Mornet <dahisland@gmail.com>
2022-01-24 06:34:33 +0100

    Add function validate() returned when form is submitted. Modal display a "thanks message" when checkbox for CGV is checked

--- js/modal.js
+++ js/modal.js
@@ -8,22 +8,46 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const spanClose = document.querySelectorAll(".close");
+const modalbg = document.querySelector(".bground"); // Const for modal background
+const modalBtn = document.querySelectorAll(".modal-btn"); // Const for button lauch modal
+const formData = document.querySelectorAll(".formData"); // Const for modal datas
+const spanClose = document.querySelectorAll(".close"); // Const for span close modal
+const form = document.querySelector("form"); // Const for form
+const modalBody = document.querySelector(".modal-body"); // Const for form content
 
-// launch modal event
+// Launch modal with event : by clicking on <button class="modal-btn">
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// Close modal event
+// Close modal with event : by clicking on <span class="close">
 spanClose.forEach((span) => span.addEventListener("click", closeModal));
 
-// launch modal form
+// Function called to launch modal form by clicking on <button class="modal-btn">
 function launchModal() {
   modalbg.style.display = "block";
+  form.style.display = "block";
 }
-// Close modal form
+// Function called to close modal by clicking on <span class="close">
 function closeModal() {
   modalbg.style.display = "none";
+}
+// Function validate form onsubmit
+function validate(event) {
+  // if all conditions are respected
+  if (checkbox1.checked == false) {
+    event.preventDefault(); // Prevent defalut reloading page after submit
+    alert("Vous devez accepter les conditions d'utilisation");
+  } else {
+    event.preventDefault(); // Prevent defalut reloading page after submit
+    form.style.display = "none"; // form is hidden to display new content
+    // Insert content in HTMl to display a message of thanks
+    modalBody.innerHTML =
+      '<p class="txt-thanks">Merci pour votre inscription<p><button class="btn-close">Fermer</button>';
+    // Close modal with event : by clicking on <button class="btn-close">
+    const buttonClose = document.querySelectorAll(".btn-close"); // const for button close for "thanks" message
+    buttonClose.forEach((btnClose) =>
+      btnClose.addEventListener("click", () => {
+        closeModal();
+      })
+    );
+  }
 }
